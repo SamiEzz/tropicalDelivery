dce6865f01916d61d0ad9ba5c1c42b295a216bd9
Sami Ezzerouali <sami.ezzerouali@amadeus.com>
2021-06-10 15:24:48 +0200

    home page

--- tropicalDelivery/mainPage/urls.py
+++ tropicalDelivery/mainPage/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('p/', views.getProducts, name='products'),
     path('products/', views.products, name='products'),
-    path('home/', views.home, name='home'),
-    path('.well-known/pki-validation/87D1915D0CC29BE6B8B3CBFC8F516BC6.txt', views.ssl, name='ssl'),
-
-
+    #path('.well-known/pki-validation/87D1915D0CC29BE6B8B3CBFC8F516BC6.txt', views.ssl, name='ssl'),
 ]
