17700ddac89588294962af37c35456e1985f056e
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2021-06-10 12:37:23 +0200

    Update models.py

--- tropicalDelivery/mainPage/models.py
+++ tropicalDelivery/mainPage/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class globalConfig(models.Model):
     name = models.CharField(max_length=200,default="Tropical Deliveries")
+    year = models.CharField(max_length=200,default="2021")
     title = models.CharField(max_length=200)
     website = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
